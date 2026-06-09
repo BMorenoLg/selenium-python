be19f2fd37fa6e5cedb30228ed2675203e5c3275
BMorenoLg <49044074+BMorenoLg@users.noreply.github.com>
2026-06-09 17:23:15 -0600

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
